002fbc6a6447ca66cd28a0db3b46ffe30cfaff16
michael harrison <mikeharrison34@gmail.com>
2020-01-18 17:13:14 -0700

    get weather by zipcode

--- weather.py
+++ weather.py
@@ -36,36 +36,38 @@ welcome = "Welcome to The Weather Bot\nYou can ask me things like, 'What's the w
 for l in welcome:
    sys.stdout.write(l)
    sys.stdout.flush()
-   time.sleep(0.08)
+   time.sleep(0.02)
 
-def hello():
-    print('')
-    print("What would you like know? ")
-    the_zipcode = raw_input()
-    first_question(the_zipcode)
+# def hello():
+#     print('')
+#     the_zipcode = raw_input()
 
-hello()
+# hello()
 
-def first_question():
-    if the_zipcode == 'Help':
-        print("okay, here are your options...")
-        print('')
-        print("1. Author     2. Version    3. Location    4. How does this work?    5. More")
-        more_options = raw_input("Which would like like information on?")
-        # get_weather(the_zipcode)
-    else:
-        print("Sorry, I have not yet learned that request")
-        hello()
-
+# def first_question():
+#     if the_zipcode == 'Help':
+#         print("okay, here are your options...")
+#         print('')
+#         print("1. Author     2. Version    3. Location    4. How does this work?    5. More")
+#         more_options = raw_input("Which would like like information on?")
+#         # get_weather(the_zipcode)
+#     else:
+#         print("Sorry, I have not yet learned that request")
+#         hello()
+print('')
+print("Enter your zipcode. ")
 def get_weather():
     the_zipcode = raw_input() 
     api_key = "e70a4b7f391668d9516d1e13385a6117"
-    url = "https://api.openweathermap.org/data/2.5/forecast?zip="
-    final_url = url + the_zipcode + "&APPID=" + api_key
+    url = "https://api.openweathermap.org/data/2.5/weather?zip="
+    final_url = url + the_zipcode + ",us&APPID=" + api_key
     json_obj = urllib2.urlopen(final_url)
     data = json.load(json_obj)
 
-    for item in data['city']:
-        print "item is :" + item['albuquerque']
+    print "The current wather in " + data['name'] + " is:\n "
+    for item in data['weather']:
+        print item['description']
 
 get_weather()
+
+
